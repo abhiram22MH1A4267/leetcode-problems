100e12574a65a94e288bfcb21d9e8ea84c019019
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2024-08-08 12:19:11 +0530

    Time: 28 ms (93.64%) | Memory: 16.5 MB (84.70%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -1,20 +1,11 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-
-        if n < 1: return False
-        if n == 1: return True
-
-        good = set([2, 3, 5])
-
-        def ugly(a, base):
-            if a in good:
-                return True
-            if a % base != 0:
-                return False
-            new_a = a // base
-            return ugly(new_a, 2) or ugly(new_a, 3) or ugly(new_a, 5)
-
-        return ugly(n, 2) or ugly(n, 3) or ugly(n, 5)
-
-            
-        
+        if n < 1:
+            return False
+        while n %2 == 0:
+            n //= 2
+        while n %3 == 0:
+            n //= 3
+        while n %5 == 0:
+            n //= 5
+        return n == 1
