eff9257282d5cfe26011e4e57751847d9f74a920
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2024-11-11 07:16:05 +0530

    Time: 2 ms (70.98%) | Memory: 26.7 MB (97.12%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkPrime(int x){
+        for(int i = 2; i <= sqrt(x); i++){
+            if(x%i == 0) return 0;
+        }
+        return 1;
+    }
+    bool primeSubOperation(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            int bound;
+            if( i == 0) bound = nums[0];
+            else bound = nums[i] - nums[i-1];
+            if(bound <= 0) return 0;
+
+            int largePrime = 0;
+            for(int j = bound -1; j >= 2; j--){
+                if(checkPrime(j)){
+                    largePrime = j;
+                    break;
+                }
+            }
+            nums[i] = nums[i] - largePrime;
+        }
+        return 1;
+    }
+};
