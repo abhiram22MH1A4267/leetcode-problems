1feaddb6dcd4690cfa78813c2d0f1cec028218e1
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2024-12-14 14:54:48 +0530

    Time: 87 ms (87.72%) | Memory: 113.1 MB (79.83%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.cpp
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -3,12 +3,12 @@ public:
     long long continuousSubarrays(vector<int>& nums) {
         int n=nums.size();
         long long cnt=0;
-        multiset<int> hasX;
+        map<int, int> hasX;
         for(int l=0, r=0; r<n; r++){
-            hasX.insert(nums[r]);
-            while(l<r && *prev(hasX.end())-*hasX.begin()>2){
-                auto it=hasX.find(nums[l]);
-                hasX.erase(it);
+            hasX[nums[r]]++;
+            while(l<r && prev(hasX.end())->first-hasX.begin()->first>2){
+                int f=--hasX[nums[l]];
+                if (f==0) hasX.erase(nums[l]);
                 l++;
             }
             cnt+=r-l+1LL;
