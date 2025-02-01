0779ba29718fe29377a583baa196a664dd0e5ab9
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-02-01 07:48:01 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (25.00%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int setBits(int n){
+        int count = 0;
+        while(n){
+            count += (n&1);
+            n >>= 1;
+        }
+        return count;
+    }
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n-1; i++){
+            // int n1 = setBits(nums[i]), n2 = setBits(nums[i+1]);
+            // if(((n1&1) == 0 && (n2&1) == 0) || ((n1&1) == 1) && (n2&1) ==  1) return false;
+            if(((nums[i]&1) == 0 && (nums[i+1]&1) == 0) || ((nums[i]&1) == 1) && (nums[i+1]&1) == 1) return false;
+        }
+        return true;
+    }
+};
