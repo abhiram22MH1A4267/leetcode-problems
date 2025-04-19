dff3574013cd18a7926f7fdab3882d99dff68867
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-04-19 07:13:22 +0530

    Time: 983 ms (19.41%) | Memory: 314.9 MB (20.42%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        int mask = 0, ans = 0;
+        for(int i = 31; i >= 0; i--){
+            mask |= 1<<i;
+            unordered_set<int>myset;
+            for(int num : nums) myset.insert(mask & num);
+            int temp = ans | (1<<i);
+            for(int pre : myset){
+                if(myset.find(temp ^ pre) != myset.end()){
+                    ans = temp;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
