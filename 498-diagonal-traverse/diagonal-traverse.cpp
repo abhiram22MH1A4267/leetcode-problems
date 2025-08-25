8915ffc23435b0261a408430a18cd6fc84500e12
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-08-25 08:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (61.02%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
-        int i = 0, j = 0, flag = 0;
-        vector<int>ans;
-        while(ans.size() < (n*m)){
-            while(i >= 0 && j < m){
-                ans.push_back(mat[i][j]);
+        int i = 0, j = 0;
+        vector<int>result;
+        while(result.size() < (n * m)){
+            while(i >= 0 && j < m) {
+                result.push_back(mat[i][j]);
                 i--;
                 j++;
             }
-            if(j >= m){
+            if(j >= m) {
                 j--;
                 i += 2;
             }
-            if(i < 0){
-                i = 0;
-            }
-            while(j >= 0 && i < n){
-                ans.push_back(mat[i][j]);
+            if(i < 0) i = 0;
+            while(j >= 0 && i < n) {
+                result.push_back(mat[i][j]);
                 i++;
                 j--;
             }
@@ -30,6 +28,7 @@ public:
                 j = 0;
             }
         }
-        return ans;
+
+        return result;
     }
 };
