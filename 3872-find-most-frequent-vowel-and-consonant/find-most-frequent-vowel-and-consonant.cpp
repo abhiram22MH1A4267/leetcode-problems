487115152dec1515f2c70a2deb36cb0ca13d2c2c
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-09-13 09:03:35 +0530

    Time: 1 ms (53.88%) | Memory: 10 MB (22.40%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int maxFreqSum(string s) {
         string vowels = "aeiou";
-        unordered_map<char, int> vowelFreq, consFreq;
+        unordered_map<char, int> mp;
 
         int val1 = 0, val2 = 0;
         for(char ch : s){
+            mp[ch]++;
             if(find(vowels.begin(), vowels.end(), ch) != vowels.end()){
-                vowelFreq[ch]++;
-                val1 = max(val1, vowelFreq[ch]);
+                val1 = max(val1, mp[ch]);
             }
             else{
-                consFreq[ch]++;
-                val2 = max(val2, consFreq[ch]);
+                val2 = max(val2, mp[ch]);
             }
         }
 
